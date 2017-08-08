5b8a2b151cea3cb1de26781dbc37f43c4e348dbd
pftechsln <30786505+pftechsln@users.noreply.github.com>
2017-08-07 20:33:31 -0700

    Update MyAngularApp.js

--- Scripts/MyAngularApp.js
+++ Scripts/MyAngularApp.js
@@ -21,8 +21,9 @@ var resources = [
 // Constants: Testing Client
 var defaultClient = {
     name: 'PatientFHIR',
-    clientId: "c45f46c7-66cb-4ac5-b8d0-d66f5260e419",
+    //clientId: "c45f46c7-66cb-4ac5-b8d0-d66f5260e419",
     //clientId: "6c3586a3-a669-4944-aab1-ad5234348f56",
+    clientId: "682f041d-5ed1-4d1b-a8c4-08653678e7ca",   
     redirectUri: "https://pftechsln.github.io/",
 }
 
